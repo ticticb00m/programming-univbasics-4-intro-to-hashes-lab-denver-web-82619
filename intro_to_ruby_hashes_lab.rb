45c7d886691d0c27497d319817a61b1cb1f7d894
mohamad farahmandifard <ticticb80om@gmail.com>
2019-08-22 22:06:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = { nazanin: brat}
 end
 
 def pioneer
